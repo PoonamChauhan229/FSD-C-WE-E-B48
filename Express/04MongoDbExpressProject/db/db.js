5564037ce04a1df9205c8e85666dfd44ebff81ee
PoonamChauhan229 <poonam.chauhan229@gmail.com>
2026-08-09 10:42:11 +0530

    updated db.js

--- Express/04MongoDbExpressProject/db/db.js
+++ Express/04MongoDbExpressProject/db/db.js
@@ -3,9 +3,7 @@ const mongoose=require('mongoose')
 
 const connectDB=async()=>{
     try{
-            // await mongoose.connect('mongodb://127.0.0.1:27017/FSD-WE-B48_Mongoose')
-            // await mongoose.connect('mongodb+srv://poonam:poonam@cluster0.lukkhw2.mongodb.net/
-            // FSD-WE-B48_Mongoose')
+            // await mongoose.connect('mongodb://127.0.0.1:27017/FSD-WE-B48_Mongoose')         
 
             // using .env
             await mongoose.connect(process.env.MONGOURL)
